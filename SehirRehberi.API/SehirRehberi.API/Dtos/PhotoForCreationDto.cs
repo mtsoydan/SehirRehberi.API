8fff9dc6ddace22b73fe8228fe5d424a785ee8d2
mtsoydan <mustafataha.soydan@gmail.com>
2020-02-07 16:19:46 +0300

    Photo Dtos Added

---
+++ SehirRehberi.API/SehirRehberi.API/Dtos/PhotoForCreationDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SehirRehberi.API.Dtos
+{
+    public class PhotoForCreationDto
+    {
+    }
+}
